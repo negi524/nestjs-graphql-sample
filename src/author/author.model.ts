cd3c559fcb4bcfded76604a9a639dc6e5e83e5e0
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-06-23 21:35:29 +0900

    docs: コメント追記

--- src/author/author.model.ts
+++ src/author/author.model.ts
@@ -1,13 +1,17 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 
+// オブジェクト全体に対してdescriptionを記載することもできる
 @ObjectType({ description: 'Authorオブジェクト' })
 export class Author {
+  // GraphQLには数値型はIntとFloatがあるので、Int利用を明記する
   @Field((type) => Int, { description: 'ID' })
   id: number;
 
+  // descriptionにより、フィールドに関する説明を記載できる
   @Field((type) => String, { description: '名前' })
   name: string;
 
+  // nullableの場合もここで指定する
   @Field((type) => Int, { nullable: true, description: '年齢' })
   age?: number;
 }
--- src/author/author.resolver.ts
+++ src/author/author.resolver.ts
@@ -1,8 +1,10 @@
 import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { Author } from './author.model';
 
+// Authorに相当するスキーマを返却することを宣言している
 @Resolver((of) => Author)
 export class AuthorResolver {
+  // わかりやすさのため、データをベタ書き
   private authors: Author[] = [
     {
       id: 1,
@@ -20,15 +22,20 @@ export class AuthorResolver {
     },
   ];
 
+  // Authorの配列を返却することを宣言している
+  // descriptionにより説明文も記載可能
   @Query((returns) => [Author], { description: 'Authorを全て取得する' })
   async getAuthor() {
     return this.authors;
   }
 
+  // Authorを単体で返却することを宣言している
+  // nullableのオプションをtrueに設定しているため、Authorオブジェクトもしくはnullが返却されることがわかる
   @Query((returns) => Author, {
     nullable: true,
     description: 'ID指定でAuthorを取得する',
   })
+  // `id`というInt型の引数をリクエスト時に指定することで、idを使った絞り込みが行えることを明示している
   async findAuthor(
     @Args('id', { type: () => Int, description: '絞り込み用のID' }) id: number,
   ) {
